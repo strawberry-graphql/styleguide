21f0a9c214197e8b4f549c9394a8bc546994c0ef
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-12 16:37:36 +0000

    Initial version

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+import path from "path";
+
+import type { StorybookConfig } from "@storybook/nextjs";
+import type { Configuration } from "webpack";
+1;
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+
+  webpackFinal: async (config: Configuration) => {
+    config.module!.rules!.push({
+      test: /\.css$/i,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: { implementation: require.resolve("postcss") },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    // Return the altered config
+    return config;
+  },
+};
+
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+/** @type {import('tailwindcss').Config} */
+
+const colors = {
+  greyscale: [
+    {
+      name: "Black",
+      hex: "#0D0E12",
+      key: "black",
+    },
+    {
+      name: "G-900",
+      hex: "#1F1F1F",
+      key: "g-900",
+    },
+
+    {
+      name: "G-700",
+      hex: "#515254",
+      key: "g-700",
+    },
+    {
+      name: "G-500",
+      hex: "#8B8B8D",
+      key: "g-500",
+    },
+    {
+      name: "G-400",
+      hex: "#C5C5C6",
+      key: "g-400",
+    },
+    {
+      name: "G-100",
+      hex: "#E2E2E3",
+      key: "g-100",
+    },
+    {
+      name: "G-50",
+      hex: "#F0F1F1",
+      key: "g-50",
+    },
+    {
+      name: "White",
+      hex: "#fff",
+      key: "white",
+    },
+  ],
+  accent: [
+    {
+      name: "Strawberry",
+      hex: "#F7393D",
+      key: "strawberry",
+    },
+    {
+      name: "Yellow - D",
+      hex: "#FFE500",
+      key: "yellow",
+    },
+    {
+      name: "Yellow - L",
+      hex: "#FEAB0A",
+      key: "yellow-light",
+    },
+    {
+      name: "Green",
+      hex: "#1EB589",
+      key: "green",
+    },
+    {
+      name: "Pink",
+      hex: "#FF9FA9",
+      key: "pink",
+    },
+    {
+      name: "Blue",
+      hex: "#0094FF",
+      key: "blue",
+    },
+    {
+      name: "Magenta",
+      hex: "#EE0A78",
+      key: "magenta",
+    },
+    {
+      name: "Orange",
+      hex: "#FE620A",
+      key: "orange",
+    },
+  ],
+  code: [
+    {
+      name: "C-Green",
+      hex: "#8ED287",
+      key: "code-green",
+    },
+    {
+      name: "C-Blue",
+      hex: "#AED5FB",
+      key: "code-blue",
+    },
+    {
+      name: "C-Purple",
+      hex: "#CBABF9",
+      key: "code-purple",
+    },
+    {
+      name: "C-Pink",
+      hex: "#EA8075",
+      key: "code-pink",
+    },
+    {
+      name: "C-Orange",
+      hex: "#EAAD75",
+      key: "code-orange",
+    },
+  ],
+};
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  colors,
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      ...Object.fromEntries(
+        Object.values(colors).flatMap((colors) =>
+          colors.map((color) => [color.key, color.hex])
+        )
+      ),
+    },
+    extend: {},
+  },
+  plugins: [],
+};
