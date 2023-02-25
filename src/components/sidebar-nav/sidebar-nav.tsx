62a941fb67ebfc24c5e32272668981e4441c6f46
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-25 18:26:41 +0000

    Update sidebar

--- src/components/sidebar-nav/sidebar-nav.tsx
+++ src/components/sidebar-nav/sidebar-nav.tsx
@@ -16,19 +16,19 @@ export const SidebarNav = ({ sections }: { sections: Section[] }) => {
         return (
           <details
             key={section.name}
-            className="mb-16 pb-16 border-b border-g-100 group"
+            className="mb-16 pb-16 border-b border-g-100 dark:border-g-900 group"
           >
-            <summary className="text-g-900 typography-label list-none relative flex justify-between items-center">
+            <summary className="text-g-900 dark:text-g-50 typography-label list-none relative flex justify-between items-center cursor-pointer">
               {section.name}
 
-              <ArrowUpIcon className="text-g-500 group-open:rotate-180" />
+              <ArrowUpIcon className="text-g-500 dark:text-g-700 group-open:rotate-180" />
             </summary>
 
             <ul>
               {section.links.map((link) => (
                 <li
                   key={link.href}
-                  className="mt-16 typography-paragraph-2 font-bold text-g-500 hover:text-g-900"
+                  className="mt-16 typography-paragraph-2 font-bold text-g-500 hover:text-strawberry"
                 >
                   <Link href={link.href}>{link.name}</Link>
                 </li>
