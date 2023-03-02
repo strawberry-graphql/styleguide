26c89436dd98262246fc02fa8c587e027735ebfc
Patrick Arminio <patrick.arminio@gmail.com>
2023-03-02 00:02:32 -0600

    Side by side component

---
+++ src/components/codebox/side-by-side.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Codebox } from "./codebox";
+
+import { SideBySide } from "./side-by-side";
+
+const meta: Meta<typeof SideBySide> = {
+  title: "Components/Codebox/SideBySide",
+  component: SideBySide,
+  parameters: {},
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof SideBySide>;
+
+const pythonExample = `<pre class="shiki " style="background-color: transparent" tabindex="0"><code><span class="line"><span style="color: var(--shiki-token-keyword)">import</span><span style="color: var(--shiki-color-text)"> strawberry</span></span>
+<span class="line"></span>
+<span class="line"><span style="color: var(--shiki-token-function)">@strawberry</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-token-function)">type</span></span>
+<span class="line"><span style="color: var(--shiki-token-keyword)">class</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-function)">User</span><span style="color: var(--shiki-color-text)">:</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">    name</span><span style="color: var(--shiki-token-punctuation)">:</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-constant)">str</span></span>
+<span class="line"></span>
+<span class="line"><span style="color: var(--shiki-color-text)">    </span><span style="color: var(--shiki-token-function)">@strawberry</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-token-function)">field</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">    </span><span style="color: var(--shiki-token-keyword)">def</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-function)">is_admin</span><span style="color: var(--shiki-color-text)">(</span><span style="color: var(--shiki-token-parameter)">self</span><span style="color: var(--shiki-color-text)">) </span><span style="color: var(--shiki-token-punctuation)">-&gt;</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-constant)">bool</span><span style="color: var(--shiki-color-text)">:</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">        </span><span style="color: var(--shiki-token-keyword)">return</span><span style="color: var(--shiki-color-text)"> self</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-color-text)">name </span><span style="color: var(--shiki-token-keyword)">==</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-string-expression)">&quot;Patrick&quot;</span></span>
+<span class="line"></span></code></pre>`;
+
+const graphqlExample = `
+<pre class="shiki " style="background-color: transparent" tabindex="0"><code><span class="line"><span style="color: var(--shiki-token-keyword)">query</span><span style="color: var(--shiki-color-text)"> {</span></span>
+<span class="line"><span style="color: var(--shiki-token-comment)">  # this is a comment</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">  user(id: </span><span style="color: var(--shiki-token-constant)">1</span><span style="color: var(--shiki-color-text)">) {</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">    name</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">    email</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">  }</span></span>
+<span class="line"><span style="color: var(--shiki-color-text)">}</span></span></code></pre>
+`;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Codebox>
+          <code dangerouslySetInnerHTML={{ __html: pythonExample }} />
+        </Codebox>
+        <Codebox>
+          <code dangerouslySetInnerHTML={{ __html: graphqlExample }} />
+        </Codebox>
+      </>
+    ),
+  },
+};
