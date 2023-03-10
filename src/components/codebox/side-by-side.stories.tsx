cd2ef854045bede9440ac7ad43f4ba422dcef9e5
Patrick Arminio <patrick.arminio@gmail.com>
2023-03-10 09:19:22 +0000

    Fix side by side example

--- src/components/codebox/side-by-side.stories.tsx
+++ src/components/codebox/side-by-side.stories.tsx
@@ -13,7 +13,7 @@ const meta: Meta<typeof SideBySide> = {
 export default meta;
 type Story = StoryObj<typeof SideBySide>;
 
-const pythonExample = `<pre class="shiki " style="background-color: transparent" tabindex="0"><code><span class="line"><span style="color: var(--shiki-token-keyword)">import</span><span style="color: var(--shiki-color-text)"> strawberry</span></span>
+const pythonExample = `<span class="line"><span style="color: var(--shiki-token-keyword)">import</span><span style="color: var(--shiki-color-text)"> strawberry</span></span>
 <span class="line"></span>
 <span class="line"><span style="color: var(--shiki-token-function)">@strawberry</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-token-function)">type</span></span>
 <span class="line"><span style="color: var(--shiki-token-keyword)">class</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-function)">User</span><span style="color: var(--shiki-color-text)">:</span></span>
@@ -22,16 +22,16 @@ const pythonExample = `<pre class="shiki " style="background-color: transparent"
 <span class="line"><span style="color: var(--shiki-color-text)">    </span><span style="color: var(--shiki-token-function)">@strawberry</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-token-function)">field</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">    </span><span style="color: var(--shiki-token-keyword)">def</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-function)">is_admin</span><span style="color: var(--shiki-color-text)">(</span><span style="color: var(--shiki-token-parameter)">self</span><span style="color: var(--shiki-color-text)">) </span><span style="color: var(--shiki-token-punctuation)">-&gt;</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-constant)">bool</span><span style="color: var(--shiki-color-text)">:</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">        </span><span style="color: var(--shiki-token-keyword)">return</span><span style="color: var(--shiki-color-text)"> self</span><span style="color: var(--shiki-token-punctuation)">.</span><span style="color: var(--shiki-color-text)">name </span><span style="color: var(--shiki-token-keyword)">==</span><span style="color: var(--shiki-color-text)"> </span><span style="color: var(--shiki-token-string-expression)">&quot;Patrick&quot;</span></span>
-<span class="line"></span></code></pre>`;
+<span class="line"></span>`;
 
 const graphqlExample = `
-<pre class="shiki " style="background-color: transparent" tabindex="0"><code><span class="line"><span style="color: var(--shiki-token-keyword)">query</span><span style="color: var(--shiki-color-text)"> {</span></span>
+<span class="line"><span style="color: var(--shiki-token-keyword)">query</span><span style="color: var(--shiki-color-text)"> {</span></span>
 <span class="line"><span style="color: var(--shiki-token-comment)">  # this is a comment</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">  user(id: </span><span style="color: var(--shiki-token-constant)">1</span><span style="color: var(--shiki-color-text)">) {</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">    name</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">    email</span></span>
 <span class="line"><span style="color: var(--shiki-color-text)">  }</span></span>
-<span class="line"><span style="color: var(--shiki-color-text)">}</span></span></code></pre>
+<span class="line"><span style="color: var(--shiki-color-text)">}</span></span>
 `;
 
 export const Default: Story = {
