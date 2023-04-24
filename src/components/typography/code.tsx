69ad94bf748ef534401e1440b195cf16bb379c46
Patrick Arminio <patrick.arminio@gmail.com>
2023-04-24 16:11:33 -0600

    Update code style

--- src/components/typography/code.tsx
+++ src/components/typography/code.tsx
@@ -1,6 +1,13 @@
+import clsx from "clsx";
+
 export const Code = ({ children }: { children: React.ReactNode }) => {
   return (
-    <code className="typography-code rounded-[4px] px-[6px] py-2 text-pink bg-g-50 dark:border dark:border-g-900 dark:bg-transparency-dark dark:text-orange">
+    <code
+      className={clsx(
+        "typography-code rounded-[4px] px-[6px] py-2 ",
+        "text-strawberry bg-inline-code border border-strawberry"
+      )}
+    >
       {children}
     </code>
   );
