d6362965a2bf63c474d7c5996fa43e9c85841547
Patrick Arminio <patrick.arminio@gmail.com>
2023-06-27 23:32:27 +0100

    0.9.0

---
+++ src/components/article-header/article-header.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ArticleHeader } from "./article-header";
+
+const meta: Meta<typeof ArticleHeader> = {
+  title: "Components/Article Header",
+  component: ArticleHeader,
+  parameters: {},
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof ArticleHeader>;
+
+export const Default: Story = {
+  args: {
+    title: "This is a title",
+    duration: "5 min read",
+    date: new Date(),
+    author: {
+      name: "John Doe",
+      login: "johndoe",
+      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    },
+  },
+};
